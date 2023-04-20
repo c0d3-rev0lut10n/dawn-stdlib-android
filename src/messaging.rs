2b9b2ae11ba01bb37451c218cea5fcec161ea00b
Laurenz Werner <werner.la@protonmail.com>
2023-04-20 23:39:53 +0200

    standardize key in-/output to use hex strings

--- src/messaging.rs
+++ src/messaging.rs
@@ -21,6 +21,7 @@ use dawn_stdlib::*;
 use jni::JNIEnv;
 use jni::objects::{JByteArray, JClass, JString};
 use jni::sys::jshort;
+use hex::{encode, decode};
 use base64::{Engine as _, engine::general_purpose::STANDARD_NO_PAD as BASE64};
 use crate::{Error, SendMessage, ParseMessage};
 use crate::error;
@@ -32,9 +33,9 @@ pub extern "C" fn Java_dawn_android_LibraryConnector_sendMsg<'local> (
 	msg_type: jshort,
 	msg_string: JString<'local>,
 	msg_bytes: JByteArray<'local>,
-	remote_pubkey_kyber: JByteArray<'local>,
-	own_seckey_sig: JByteArray<'local>,
-	pfs_key: JByteArray<'local>
+	remote_pubkey_kyber: JString<'local>,
+	own_seckey_sig: JString<'local>,
+	pfs_key: JString<'local>
 ) -> JString<'local> {
 	
 	let msg_type = match u8::try_from(msg_type) {
@@ -58,17 +59,29 @@ pub extern "C" fn Java_dawn_android_LibraryConnector_sendMsg<'local> (
 		_ => Some(msg_bytes.as_slice())
 	};
 	
-	let remote_pubkey_kyber = env.convert_byte_array(remote_pubkey_kyber);
+	let remote_pubkey_kyber = env.get_string(&remote_pubkey_kyber);
 	if remote_pubkey_kyber.is_err() { error!(env, "Could not get java variable: remote_pubkey_kyber"); }
-	let remote_pubkey_kyber = remote_pubkey_kyber.unwrap();
+	let remote_pubkey_kyber: String = remote_pubkey_kyber.unwrap().into();
+	let remote_pubkey_kyber = match decode(remote_pubkey_kyber) {
+		Ok(res) => res,
+		Err(_) => { error!(env, "remote_pubkey_kyber invalid"); }
+	};
 	
-	let own_seckey_sig = env.convert_byte_array(own_seckey_sig);
+	let own_seckey_sig = env.get_string(&own_seckey_sig);
 	if own_seckey_sig.is_err() { error!(env, "Could not get java variable: own_seckey_sig"); }
-	let own_seckey_sig = own_seckey_sig.unwrap();
+	let own_seckey_sig: String = own_seckey_sig.unwrap().into();
+	let own_seckey_sig = match decode(own_seckey_sig) {
+		Ok(res) => res,
+		Err(_) => { error!(env, "own_seckey_sig invalid"); }
+	};
 	
-	let pfs_key = env.convert_byte_array(pfs_key);
+	let pfs_key = env.get_string(&pfs_key);
 	if pfs_key.is_err() { error!(env, "Could not get java variable: pfs_key"); }
-	let pfs_key = pfs_key.unwrap();
+	let pfs_key: String = pfs_key.unwrap().into();
+	let pfs_key = match decode(pfs_key) {
+		Ok(res) => res,
+		Err(_) => { error!(env, "pfs_key invalid"); }
+	};
 	
 	let (new_pfs_key, mdc, ciphertext) = match send_msg((msg_type, msg_string, msg_bytes), &remote_pubkey_kyber, &own_seckey_sig, &pfs_key) {
 		Ok(res) => res,
@@ -76,7 +89,7 @@ pub extern "C" fn Java_dawn_android_LibraryConnector_sendMsg<'local> (
 	};
 	let send_message = SendMessage {
 		status: "ok",
-		new_pfs_key: &BASE64.encode(new_pfs_key),
+		new_pfs_key: &encode(new_pfs_key),
 		mdc: &mdc,
 		ciphertext: &BASE64.encode(ciphertext)
 	};
@@ -93,35 +106,50 @@ pub extern "C" fn Java_dawn_android_LibraryConnector_sendMsg<'local> (
 
 #[no_mangle]
 pub extern "C" fn Java_dawn_android_LibraryConnector_parseMsg<'local> (
-	env: JNIEnv<'local>,
+	mut env: JNIEnv<'local>,
 	_class: JClass<'local>,
 	msg_ciphertext: JByteArray<'local>,
-	own_seckey_kyber: JByteArray<'local>,
-	remote_pubkey_sig: JByteArray<'local>,
-	pfs_key: JByteArray<'local>
+	own_seckey_kyber: JString<'local>,
+	remote_pubkey_sig: JString<'local>,
+	pfs_key: JString<'local>
 ) -> JString<'local> {
 	
 	let msg_ciphertext = env.convert_byte_array(msg_ciphertext);
 	if msg_ciphertext.is_err() { error!(env, "Could not get java variable: msg_ciphertext"); }
 	let msg_ciphertext = msg_ciphertext.unwrap();
 	
-	let own_seckey_kyber = env.convert_byte_array(own_seckey_kyber);
+	let own_seckey_kyber = env.get_string(&own_seckey_kyber);
 	if own_seckey_kyber.is_err() { error!(env, "Could not get java variable: own_seckey_kyber"); }
-	let own_seckey_kyber = own_seckey_kyber.unwrap();
-	
-	let remote_pubkey_sig = env.convert_byte_array(remote_pubkey_sig);
-	if remote_pubkey_sig.is_err() { error!(env, "Could not get java variable: remote_pubkey_sig"); }
-	let remote_pubkey_sig = remote_pubkey_sig.unwrap();
-	let remote_pubkey_sig = match remote_pubkey_sig.len() {
-		0 => None,
-		_ => Some(remote_pubkey_sig.as_slice())
+	let own_seckey_kyber: String = own_seckey_kyber.unwrap().into();
+	let own_seckey_kyber = match decode(own_seckey_kyber) {
+		Ok(res) => res,
+		Err(_) => { error!(env, "own_seckey_kyber invalid"); }
 	};
 	
-	let pfs_key = env.convert_byte_array(pfs_key);
-	if pfs_key.is_err() { error!(env, "Could not get java variable: pfs_key"); }
-	let pfs_key = pfs_key.unwrap();
+	let remote_pubkey_sig = env.get_string(&remote_pubkey_sig);
+	if remote_pubkey_sig.is_err() { error!(env, "Could not get java variable: remote_pubkey_sig"); }
+	let remote_pubkey_sig: String = remote_pubkey_sig.unwrap().into();
+	let optional_remote_pubkey_sig;
+	let remote_pubkey_sig_decoded;
+	if &remote_pubkey_sig == "" {
+		optional_remote_pubkey_sig = None;
+	}
+	else {
+		let remote_pubkey_sig_res = decode(remote_pubkey_sig);
+		if remote_pubkey_sig_res.is_err() { error!(env, "remote_pubkey_sig invalid"); }
+		remote_pubkey_sig_decoded = remote_pubkey_sig_res.unwrap();
+		optional_remote_pubkey_sig = Some(remote_pubkey_sig_decoded.as_slice());
+	}
 	
-	let ((msg_type, msg_text, msg_bytes), new_pfs_key, mdc) = match parse_msg(&msg_ciphertext, &own_seckey_kyber, remote_pubkey_sig, &pfs_key) {
+	let pfs_key = env.get_string(&pfs_key);
+	if pfs_key.is_err() { error!(env, "Could not get java variable: pfs_key"); }
+	let pfs_key: String = pfs_key.unwrap().into();
+	let pfs_key = match decode(pfs_key) {
+		Ok(res) => res,
+		Err(_) => { error!(env, "pfs_key invalid"); }
+	};
+	
+	let ((msg_type, msg_text, msg_bytes), new_pfs_key, mdc) = match parse_msg(&msg_ciphertext, &own_seckey_kyber, optional_remote_pubkey_sig, &pfs_key) {
 		Ok(res) => res,
 		Err(err) => { error!(env, &err); }
 	};
@@ -141,7 +169,7 @@ pub extern "C" fn Java_dawn_android_LibraryConnector_parseMsg<'local> (
 		msg_type,
 		msg_text: &msg_text,
 		msg_bytes: &msg_bytes,
-		new_pfs_key: &BASE64.encode(new_pfs_key),
+		new_pfs_key: &encode(new_pfs_key),
 		mdc: &mdc,
 	};
 	
